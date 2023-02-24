33e70d17e80201e62c922665fe6b5568c4d20ba2
Gethin-a <126208350+Gethin-a@users.noreply.github.com>
2023-02-24 20:58:01 +0000

    Add files via upload

---
+++ discordrichtext-master/client.lua
@@ -0,0 +1,63 @@
+------created by gothing#7524------
+
+Citizen.CreateThread(function()
+	while true do
+		SetDiscordAppId(1078322108066705540) 
+		SetDiscordRichPresenceAsset('image1') 
+        SetDiscordRichPresenceAssetText('Come and join us!') 
+        SetDiscordRichPresenceAssetSmall('image2')
+        SetDiscordRichPresenceAssetSmallText('Verified Fun!!')
+    SetDiscordRichPresenceAction(0, "Discord!", "https://discord.gg/Vj7Xxz92ED")
+    SetDiscordRichPresenceAction(1, "FiveM!", "fivem://connect/134.255.234.40")
+		Citizen.Wait(30*1000) 
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		local VehName = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))))
+		if VehName == "NULL" then VehName = GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))) end
+		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId(),true))
+		local StreetHash = GetStreetNameAtCoord(x, y, z)
+		local pId = GetPlayerServerId(PlayerId())
+		local pName = GetPlayerName(PlayerId())
+		local playerCount = GetActivePlayers()
+		local maxPlayerSlots = "46"
+		Citizen.Wait(10000)
+		if StreetHash ~= nil then
+			StreetName = GetStreetNameFromHashKey(StreetHash)
+			if IsPedOnFoot(PlayerPedId()) and not IsEntityInWater(PlayerPedId()) then
+				if IsPedSprinting(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | sprinting down "..StreetName)
+				elseif IsPedRunning(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | running down "..StreetName)
+				elseif IsPedWalking(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | is walking down "..StreetName)
+				elseif IsPedStill(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | standing on "..StreetName)
+				end
+			elseif GetVehiclePedIsUsing(PlayerPedId()) ~= nil and not IsPedInAnyHeli(PlayerPedId()) and not IsPedInAnyPlane(PlayerPedId()) and not IsPedOnFoot(PlayerPedId()) and not IsPedInAnySub(PlayerPedId()) and not IsPedInAnyBoat(PlayerPedId()) then
+				local MPH = math.ceil(GetEntitySpeed(GetVehiclePedIsUsing(PlayerPedId())) * 2.236936)
+				if MPH > 50 then
+					SetRichPresence("ID: "..pId.." | speeding down "..StreetName.." at "..MPH.."MPH in a "..VehName)
+				elseif MPH <= 50 and MPH > 0 then
+					SetRichPresence("ID: "..pId.." | cruising down "..StreetName.." at "..MPH.."MPH in a "..VehName)
+				elseif MPH == 0 then
+					SetRichPresence("ID: "..pId.." | parked on "..StreetName.." in a "..VehName)
+				end
+			elseif IsPedInAnyHeli(PlayerPedId()) or IsPedInAnyPlane(PlayerPedId()) then
+				if IsEntityInAir(GetVehiclePedIsUsing(PlayerPedId())) or GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId())) > 5.0 then
+					SetRichPresence("ID: "..pId.." | flying over "..StreetName.." in a "..VehName)
+				else
+					SetRichPresence("ID: "..pId.." | landed at "..StreetName.." in a "..VehName)
+				end
+			elseif IsEntityInWater(PlayerPedId()) then
+				SetRichPresence("ID: "..pId.." | "..pName.." is swimming")
+			elseif IsPedInAnyBoat(PlayerPedId()) and IsEntityInWater(GetVehiclePedIsUsing(PlayerPedId())) then
+				SetRichPresence("ID: "..pId.." | "..pName.." is sailing in a "..VehName)
+			elseif IsPedInAnySub(PlayerPedId()) and IsEntityInWater(GetVehiclePedIsUsing(PlayerPedId())) then
+				SetRichPresence("ID: "..pId.." | "..pName.." is in a yellow submarine")
+			end
+		end
+	end
+end)
---
+++ discordrichtext-master/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Gothing#7524'
+description 'discord presence'
+
+client_script 'client.lua'
